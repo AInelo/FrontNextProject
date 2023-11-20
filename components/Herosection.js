d5fe1666d6663fc35655e58f2ab63ca1cecdb9d9
TOTON Lionel <totonlionel@gmail.com>
2023-11-20 01:21:52 +0100

    Voici la version cool

--- components/Herosection.js
+++ components/Herosection.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Herosection() {
 	return (
-		<div className="my-4 z-0">
+		<div className="my-4">
 			<h1 className="md:text-8xl text-4xl font-bold text-center text-gray-900">
 				Responsive<span className="text-blue-700"> Footer</span> using{" "}
 				<span className="text-blue-700">Nextjs</span> &{" "}
