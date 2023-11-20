bff1cc0ab4ae44befa0ebe471411b036b8085006
TOTON Lionel <totonlionel@gmail.com>
2023-11-20 11:10:47 +0100

    Le dernier commit sur les newsletter et les articles section

--- components/Herosection.js
+++ components/Herosection.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Herosection() {
 	return (
-		<div className="my-4">
+		<div className="my-4 bg-slate-800">
 			<h1 className="md:text-8xl text-4xl font-bold text-center text-gray-900">
 				Responsive<span className="text-blue-700"> Design</span> Faits{" "}
 				<span className="text-blue-700">TOTON</span> &{" "}
