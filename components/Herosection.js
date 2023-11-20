e93800310aef0c9442835f8cbdaeb3603c924a24
TOTON Lionel <totonlionel@gmail.com>
2023-11-20 10:02:41 +0100

    Commit fait depuis le Chroombook

--- components/Herosection.js
+++ components/Herosection.js
@@ -4,9 +4,9 @@ function Herosection() {
 	return (
 		<div className="my-4">
 			<h1 className="md:text-8xl text-4xl font-bold text-center text-gray-900">
-				Responsive<span className="text-blue-700"> Footer</span> using{" "}
-				<span className="text-blue-700">Nextjs</span> &{" "}
-				<span className="text-blue-700">Tailwindcss</span>
+				Responsive<span className="text-blue-700"> Design</span> Faits{" "}
+				<span className="text-blue-700">TOTON</span> &{" "}
+				<span className="text-blue-700">Lionel</span>
 			</h1>
 		</div>
 	);
