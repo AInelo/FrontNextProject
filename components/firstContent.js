b81c72698e572ff8fc51a18dc1175c2c527fdd83
TOTON Lionel <totonlionel@gmail.com>
2023-11-20 11:33:56 +0100

    voici l'ajout de tout les elements au sein de notre site ecom

--- components/firstContent.js
+++ components/firstContent.js
@@ -517,8 +517,10 @@ function FirstContent() {
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="sr-only">Produits disponible</h2>
+          <h2 className="text-black sr-only">Produits disponible</h2>
   
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+
             {products.map((product) => (
               <a key={product.id} href={product.href} className="group">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
@@ -532,6 +534,7 @@ function FirstContent() {
                 <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
               </a>
             ))}
+
           </div>
         </div>
       </div>
